16c3180c5b9313b082d0b1f9dcac5fade4dc0670
Awais Chorahi <36324342+awaischorahi@users.noreply.github.com>
2021-09-01 17:28:30 +0500

    commit

--- app/src/main/java/com/applicationdevelopers/fitnessx/Views/SignUp/SignUp.java
+++ app/src/main/java/com/applicationdevelopers/fitnessx/Views/SignUp/SignUp.java
@@ -2,18 +2,29 @@ package com.applicationdevelopers.fitnessx.Views.SignUp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.applicationdevelopers.fitnessx.R;
+import com.applicationdevelopers.fitnessx.Views.ProfileDetails.CompleteProfile;
 
 
 public class SignUp extends AppCompatActivity {
-
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        register=findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CompleteProfile.class));
+            }
+        });
     }
 }
